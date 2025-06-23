fe3bc001afd75049023085ffd52f84cf3811eaad
Uttam singh <anshuchauhan1611@gmail.com>
2025-06-23 18:38:39 +0530

    Solved bug api error

--- weather_project/weather/views.py
+++ weather_project/weather/views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 class WeatherView(APIView):
     def get(self, request):
         city = request.GET.get('city', 'London')
-        api_key = 'YOUR_API_KEY'
+        api_key = '4d0166f3ea050e398272a82bebcb2ce4'
         url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric'
         resp = requests.get(url)
         data = resp.json()
--- weather_project/weather_project/asgi.py
+++ weather_project/weather_project/asgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import weather.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_project.settings')
 
 application = get_asgi_application()
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_project.settings')
+
+application = ProtocolTypeRouter(
+    {
+        "http": get_asgi_application(),
+        "websocket": AuthMiddlewareStack(
+            URLRouter(
+                weather.routing.websocket_urlpatterns
+            )
+        ),
+    }
+)
+
